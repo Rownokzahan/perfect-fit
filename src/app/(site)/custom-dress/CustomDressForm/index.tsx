b19b6300d068f6a9b7e966f382ea087b4aadb336
Rownok Zahan <rownokzahan9@gmail.com>
2026-03-04 14:41:52 +0600

    Implement Custom Dress Designer; add DressDesigner component, integrate measurement inputs, special requests, and form submission handling for enhanced user experience

---
+++ src/app/(site)/custom-dress/CustomDressForm/index.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import MeasurementInputsSection from "@/components/forms/components/customize/MeasurementInputsSection";
+import SpecialRequest from "@/components/forms/components/customize/SpecialRequest";
+import FormSubmitButton from "@/components/forms/components/FormSubmitButton";
+import { CustomizationFormData } from "@/types";
+import { useForm } from "react-hook-form";
+import DressDesigner from "./DressDesigner";
+import useModalById from "@/hooks/useModalById";
+import { useTransition } from "react";
+import { AddToCartPayload } from "@/types/cart";
+import { addToCart } from "@/actions/cart/mutations/addToCart";
+import toast from "react-hot-toast";
+
+const CustomDressForm = () => {
+  const [isPending, startTransition] = useTransition();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<CustomizationFormData>();
+
+  const { openModal } = useModalById("addToCartModal");
+
+  const handleAddToCart = (data: CustomizationFormData) => {
+    const cartItem: AddToCartPayload = {
+      customizedProduct: {
+        productType: "customDress",
+
+        bodiceType: data.bodiceType,
+        sleeveType: data.sleeveType,
+        skirtType: data.skirtType,
+        fabric: data.fabric,
+
+        length: data.length,
+        sleeveLength: data.sleeveLength,
+        chest: data.chest,
+        waist: data.waist,
+
+        request: data.request,
+      },
+
+      name: "Custom Dress",
+      quantity: 1,
+      unitPrice: 50,
+      subtotal: 50,
+    };
+
+    startTransition(async () => {
+      const error = await addToCart(cartItem);
+      if (error) {
+        toast.error(error.message, { duration: 5000 });
+      } else {
+        reset();
+        openModal();
+      }
+    });
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(handleAddToCart)}
+      className="bg-light space-y-6"
+    >
+      <DressDesigner register={register} watch={watch} errors={errors} />
+
+      <MeasurementInputsSection register={register} errors={errors} />
+      <SpecialRequest register={register} />
+
+      <FormSubmitButton label="Add to Cart" isFormSubmitting={isPending} />
+    </form>
+  );
+};
+
+export default CustomDressForm;
